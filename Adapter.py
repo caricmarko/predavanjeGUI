dc005bd3d13d87f2e39bcae3a7632286f85216e0
unknown <caric.marko@gmail.com>
2022-06-02 19:34:13 +0200

    Primer Adapter

---
+++ Adapter.py
@@ -0,0 +1,57 @@
+import socket
+#import syslog
+import logging
+
+# Ovde se ne prave klase za specificne greske (i eventualno njihove podklase)
+
+# The initial class.
+
+class Logger(object):
+    def __init__(self, file):
+        self.file = file
+
+    def log(self, message):
+        self.file.write(message + '\n')
+        self.file.flush()
+
+# New design direction: filtering messages.
+
+class FilteredLogger(Logger):
+    def __init__(self, pattern, file):
+        self.pattern = pattern
+        super().__init__(file)
+
+    def log(self, message):
+        if self.pattern in message:
+            super().log(message)
+
+class FileLikeSocket:
+    def __init__(self, sock):
+        self.sock = sock
+
+    def write(self, message_and_newline):
+        self.sock.sendall(message_and_newline.encode('ascii'))
+
+    def flush(self):
+        pass
+
+class FileLikeSyslog:
+    def __init__(self, priority):
+        self.priority = priority
+
+    def write(self, message_and_newline):
+        message = message_and_newline.rstrip('\n')
+        logging.error(message)
+        #syslog.syslog(self.priority, message)
+
+    def flush(self):
+        pass
+
+sock1, sock2 = socket.socketpair()
+
+fs = FileLikeSocket(sock1)
+logger = FilteredLogger('Error', fs)
+logger.log('Warning: message number one')
+logger.log('Error: message number two')
+
+print('The socket received: %r' % sock2.recv(512))
---
+++ EksplozijaKlasa.py
@@ -0,0 +1,47 @@
+import sys
+#import syslog
+import logging
+
+# The initial class.
+
+class Logger(object):
+    def __init__(self, file):
+        self.file = file
+
+    def log(self, message):
+        self.file.write(message + '\n')
+        self.file.flush()
+
+# Two more classes, that send messages elsewhere.
+
+class SocketLogger(Logger):
+    def __init__(self, sock):
+        self.sock = sock
+
+    def log(self, message):
+        self.sock.sendall((message + '\n').encode('ascii'))
+
+class SyslogLogger(Logger):
+    def __init__(self, priority):
+        self.priority = priority
+
+    def log(self, message):
+        logging.error(message)
+        #syslog.syslg(self.priority, message)
+
+# New design direction: filtering messages.
+
+class FilteredLogger(Logger):
+    def __init__(self, pattern, file):
+        self.pattern = pattern
+        super().__init__(file)
+
+    def log(self, message):
+        if self.pattern in message:
+            super().log(message)
+
+# It works.
+
+f = FilteredLogger('Error', sys.stdout)
+f.log('Ignored: this is not important')
+f.log('Error: but you want to see this')
