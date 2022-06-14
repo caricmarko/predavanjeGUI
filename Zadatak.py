8a5df892d985f59091d8781db7ef141f2b34afd4
Nikola Mandic <markizmandic@icloud.com>
2022-06-14 18:22:41 +0200

    dorada zadatka

--- Zadatak.py
+++ Zadatak.py
@@ -16,3 +16,19 @@ for i in range(len(novi)//2):
     (novi[i],novi[-1-i])=(novi[-1-i],novi[i])
 print(novi)
 
+# proba
+a = [-5,-3,0,1,4,5]
+poc = 0 #indeks levo
+kraj = len(a)-1 #indeks desno
+index = len(a)-1
+
+novi = [0 for i in a]
+while(index>=0):
+    if(abs(a[poc])>abs(a[kraj])):
+        novi[index] = (a[poc]*a[poc])
+        poc+=1
+    else:
+        novi[index] = (a[kraj]*a[kraj])
+        kraj-=1
+    index -= 1
+print(novi)
